2509968ef56e125db33d9c415fa057d561125b1c
Michael Rüegg <rueegg.michael@gmail.com>
2016-03-04 13:55:03 +0100

    Fixed SHIPIT-4

--- src/main/scala/ch/mibex/bamboo/shipit/mpac/MpacFacade.scala
+++ src/main/scala/ch/mibex/bamboo/shipit/mpac/MpacFacade.scala
@@ -14,14 +14,14 @@ import scala.collection.JavaConverters._
 
 case class MpacCredentials(vendorUserName: String, vendorPassword: String)
 
-case class NewPluginVersion(plugin: Plugin,
-                            fromVersion: PluginVersion,
-                            buildNumber: Int,
-                            versionNumber: String,
-                            binary: Deployment,
-                            isPublicVersion: Boolean,
-                            releaseSummary: String,
-                            releaseNotes: String)
+case class NewPluginVersionDetails(plugin: Plugin,
+                                   oldVersion: PluginVersion,
+                                   buildNumber: Int,
+                                   versionNumber: String,
+                                   binary: Deployment,
+                                   isPublicVersion: Boolean,
+                                   releaseSummary: String,
+                                   releaseNotes: String)
 
 sealed trait MpacError {
   def i18n: String
@@ -52,6 +52,7 @@ object MpacFacade {
       .credentials(UPMOption.some(c))
       .build()
     val client = new DefaultMarketplaceClient(AbstractMarketplaceClient.DEFAULT_SERVER_URI, httpConfig)
+
     try {
       block(new MpacFacade(client))
     } finally {
@@ -63,21 +64,38 @@ object MpacFacade {
 
 
 class MpacFacade(client: MarketplaceClient) extends Logging {
+  private final val LinksToCopy = List("issue-tracker", "documentation", "license", "eula")
 
-  def findPlugin(pluginKey: String): Option[Plugin] = {
-    val criteria = PluginDetailQuery.builder(pluginKey).build()
-    client.plugins().get(criteria)
+  def findPlugin(pluginKey: String): Either[MpacError, Option[Plugin]] = {
+    try {
+      val criteria = PluginDetailQuery.builder(pluginKey).build()
+      Right(client.plugins().get(criteria))
+    } catch {
+      case e: MpacException.ServerError if e.getStatus == 401 || e.getStatus == 403 =>
+        log.error(s"SHIPIT2MARKETPLACE: failed to find plug-in with key $pluginKey", e)
+        Left(MpacAuthenticationError())
+      case e: MpacException.ConnectionFailure =>
+        log.error(s"SHIPIT2MARKETPLACE: failed to find plug-in with key $pluginKey", e)
+        Left(MpacConnectionError())
+    }
   }
 
-  def publish(version: NewPluginVersion): Either[MpacUploadError, PluginVersion] = {
+  def publish(version: NewPluginVersionDetails): Either[MpacError, PluginVersion] = {
+
     val versionBuilder =
       PluginVersionUpdate
-        .copyNewVersion(version.plugin, version.fromVersion, version.buildNumber, version.versionNumber, version.binary)
+        .copyNewVersion(version.plugin, version.oldVersion, version.buildNumber, version.versionNumber, version.binary)
         .published(version.isPublicVersion)
         .summary(UPMOption.option(version.releaseSummary))
         .releaseNotes(UPMOption.some(version.releaseNotes))
 
-    for (compat <- version.fromVersion.getCompatibilities.asScala) {
+    // we cannot just take all links because some of them are internal Marketplace links
+    // and because there are duplicate links which result in an error when uploading a new version
+    for (l <- version.oldVersion.getLinks.getItems.asScala if LinksToCopy contains l.getRel) {
+      versionBuilder.addLink(l.getRel, l.getHref)
+    }
+
+    for (compat <- version.oldVersion.getCompatibilities.asScala) {
       val maxVersion = compat.getMax.getVersion
       val minVersion = compat.getMin.getVersion
       val applicationName = ApplicationKey.valueOf(compat.getApplicationName)
@@ -87,7 +105,14 @@ class MpacFacade(client: MarketplaceClient) extends Logging {
     try {
       Right(client.plugins().putVersion(versionBuilder.build()))
     } catch {
+      case e: MpacException.ServerError if e.getStatus == 401 || e.getStatus == 403 =>
+        log.error(s"SHIPIT2MARKETPLACE: failed to publish plug-in", e)
+        Left(MpacAuthenticationError())
+      case e: MpacException.ConnectionFailure =>
+        log.error(s"SHIPIT2MARKETPLACE: failed to publish plug-in", e)
+        Left(MpacConnectionError())
       case e: MpacException =>
+        log.error(s"SHIPIT2MARKETPLACE: failed to publish plug-in", e)
         val reason = Utils.mapFromJson(e.getMessage)
           .get("errors")
           .map(e => e.asInstanceOf[List[String]].mkString(", "))
@@ -102,8 +127,12 @@ class MpacFacade(client: MarketplaceClient) extends Logging {
       client.applications().findAll()
       None
     } catch {
-      case e: MpacException.ServerError if e.getStatus == 401 => Some(MpacAuthenticationError())
-      case e: Exception => Some(MpacConnectionError())
+      case e: MpacException.ServerError if e.getStatus == 401 || e.getStatus == 403 =>
+        log.error(s"SHIPIT2MARKETPLACE: failed to check credentials", e)
+        Some(MpacAuthenticationError())
+      case e: MpacException.ConnectionFailure =>
+        log.error(s"SHIPIT2MARKETPLACE: failed to check credentials", e)
+        Some(MpacConnectionError())
     }
   }
 
